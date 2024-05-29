658ea5919e85cb80278b9c484c535fc7a4e8b693
Guilherme Henrique <a226881906@fumec.edu.br>
2024-05-29 14:29:02 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import fastify from "fastify";
+import * as dotenv from 'dotenv';
+import fetch from "node-fetch";
+
+dotenv.config();
+const app = fastify();
+
+app.post("/get-info", async (request, reply) => {
+    const body: getBody = request.body as getBody;
+    const url = `https://${process.env.BLYNK_REGION}${process.env.GET_URL}${process.env.TOKEN}${body.virtualPort}`;
+
+    const response = await fetch(url);
+    const data = await response.text();
+
+    reply.send(data);
+});
+
+app.post("/update", async (request, reply) => {
+    const body: updateBody = request.body as updateBody;
+    const url = `https://${process.env.BLYNK_REGION}${process.env.UPDATE_URL}${process.env.TOKEN}${body.virtualPort}=${body.value}`;
+
+    const response = await fetch(url);
+    const answer = await response.status;
+
+    reply.send(`Status Code: ${answer}`);
+});
+
+app.listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3005,
+})
+.then(() => {
+    console.log("HTTP Server Running");
+});
+
+interface getBody {
+    virtualPort: string
+}
+
+interface updateBody {
+    virtualPort: string,
+    value: string
+}
